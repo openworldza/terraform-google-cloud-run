24fb564c8dbb222e7f4591f1fc4fbf28c760aa49
Gareth Allen <gallen@openworld.co.za>
2023-01-07 20:05:24 +0200

    Add lifecycle

--- variables.tf
+++ variables.tf
@@ -231,11 +231,3 @@ variable "members" {
   description = "Users/SAs to be given invoker access to the service"
   default     = []
 }
-
-
-variable "lifecycle_ignore_changes" {
-  type        = list
-  description = "Lifecycle ignore changes"
-  default     = []
-
-}
